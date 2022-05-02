0b85082cd2525ab1ae7d15dd758cbab20b398148
RoxyKko <86786320+RoxyKko@users.noreply.github.com>
2022-05-03 00:05:06 +0800

    Add files via upload

---
+++ demo3/RunDemo5.py
@@ -0,0 +1,14 @@
+import sys
+import demo5
+
+from PyQt5.QtWidgets import QApplication,QMainWindow
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainWindow = QMainWindow()
+    ui = demo5.Ui_MainWindow()
+    ui.setupUi(mainWindow)
+    mainWindow.show()
+    sys.exit(app.exec_())
+    #控件尺寸最大最小值、尺寸策略
+    #我们得到按钮控件的期望尺寸（sizeHint）为32*23（宽*高）
